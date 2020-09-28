6c25e7bce962f757426c8c8fe8535e7ba5f417f9
Igor Alpoim O. Puppim <igor.puppim@gmail.com>
2020-09-27 21:09:58 -0300

    Finalizado m[odulo 6

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,23 @@ import React from "react"
 
 import Layout from "../components/Layout"
 import SEO from "../components/seo"
+import PostItem from "../components/PostItem"
+
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1>Home</h1>
-  
+    <PostItem
+      slug="/about/"
+      background="#bada55"
+      category="misc"
+      date="27 de setembro"
+      timeToRead="5"
+      title="Diga n&atilde;o ao framework javascript desnecess&aacute;rio"
+      description="Um post hipocr&iacute;ta constru&iacute;do com framwork"
+    ></PostItem>
+    
   </Layout>
 )
 
